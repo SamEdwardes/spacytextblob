f9ff4a4673a378320a905b14e228ee2f659940bd
SamEdwardes <edwardes.s@gmail.com>
2020-11-11 21:23:58 -0800

    added sentiment analysis on the sentence level

--- spacytextblob/textblob.py
+++ spacytextblob/textblob.py
@@ -1,4 +1,5 @@
-from spacy.tokens import Doc
+from spacy.tokens import Doc, Span
+
 from textblob import TextBlob
 
 
@@ -9,12 +10,25 @@ class SpacyTextBlob(object):
         Doc.set_extension("polarity", default=None)
         Doc.set_extension("subjectivity", default=None)
         Doc.set_extension("assessments", default=None)
+        Span.set_extension("polarity", default=None)
+        Span.set_extension("subjectivity", default=None)
+        Span.set_extension("assessments", default=None)
 
     def __call__(self, doc):
-
+        # sentiment at the doc level
         blob = TextBlob(doc.text)
         sentiment = blob.sentiment_assessments
         doc._.set("polarity", sentiment.polarity)
         doc._.set("subjectivity", sentiment.subjectivity)
         doc._.set("assessments", sentiment.assessments)
+        
+        # sentiment on the sentence level
+        for span in doc.sents:
+            blob = TextBlob(span.text)
+            sentiment = blob.sentiment_assessments
+            span._.set("polarity", sentiment.polarity)
+            span._.set("subjectivity", sentiment.subjectivity)
+            span._.set("assessments", sentiment.assessments)
+        
+        # sentiment at the sentence level
         return doc
