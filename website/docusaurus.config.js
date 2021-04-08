e5a69347a67c445984fdbfbcdb74299dcb694578
Gabriel Flynn <gm.flynn99@yahoo.com>
2021-04-08 00:09:32 -0500

    Update edit url in docusaurus config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -73,13 +73,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/samedwardes/spacytextblob/edit/master/website/',
+            'https://github.com/samedwardes/spacytextblob/edit/main/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/samedwardes/spacytextblob/edit/master/website/blog/',
+            'https://github.com/samedwardes/spacytextblob/edit/main/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
